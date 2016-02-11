5609c1457ddf7f950991b2ad9de46d6c757d6398
ayrokid <ayrokid@gmail.com>
2016-02-11 18:51:42 +0700

    add radius maps

--- app/src/main/java/com/silvanix/maps/maps/MapsActivity.java
+++ app/src/main/java/com/silvanix/maps/maps/MapsActivity.java
@@ -2,16 +2,23 @@ package com.silvanix.maps.maps;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -20,6 +27,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap gmap;
     private Marker maker;
+    private Circle mCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -44,11 +53,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
-
+        double radiusInMeters = 2000; //2 kilometer
+        //blue outline
+        int strokeColor = 0xff0000ff;
+        //opaque blue fill
+        int shadeColor = 0x550000FF;
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng position = new LatLng(-34, 151);
+        CircleOptions circleOptions = new CircleOptions()
+                                    .center(position)
+                                    .radius(radiusInMeters)
+                                    .fillColor(shadeColor)
+                                    .strokeColor(strokeColor)
+                                    .strokeWidth(0f);
+        gmap.addCircle(circleOptions);
+
+        gmap.addMarker(new MarkerOptions().position(position).title("Marker in Sydney"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        gmap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -72,11 +94,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
+    public void firstButtonClick(View v) {
+        Toast.makeText(this, "Clicked on Button 1", Toast.LENGTH_LONG).show();
+    }
 
-    private void addMaker(GoogleMap map, double lat, double lon, int title, int snippet) {
-        map.addMarker(new MarkerOptions().position(new LatLng(lat, lon))
-                .title(getString(title))
-                .snippet(getString(snippet))
-        );
+    public void secondButtonClick(View v) {
+        Toast.makeText(this, "Clicked on Button 2", Toast.LENGTH_LONG).show();
+    }
+
+    public void thirdButtonClick(View v) {
+        Toast.makeText(this, "Clicked on Button 3", Toast.LENGTH_LONG).show();
     }
 }
